b48e32e26a9337a3915a65957a9d4bd7c5cfb01b
Bimal.Patel <bimal.patel@yahoo.com>
2024-02-02 14:19:39 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+output "availability_zones" {
+  description = "The availability zones of the instance."
+  value       = aws_rds_cluster.cluster.availability_zones
+}
 output "aurora_postgres_endpoint" {
+  description = "The DNS address of the RDS instance."
   value = aws_db_instance.aurora_postgres.endpoint
+}
+output "reader_endpoint" {
+  description = "A read only endpoint for the cluster, automatically load balanced across replicas."
+  value       = aws_rds_cluster.cluster.reader_endpoint
 }
