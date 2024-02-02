99fb6da60bd1d15c743c1072ec3414aec78c6879
Bimal.Patel <bimal.patel@yahoo.com>
2024-02-02 11:09:59 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "db_name" {
+  description = "Name of the Aurora PostgreSQL database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for the Aurora PostgreSQL database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the Aurora PostgreSQL database"
+  type        = string
+}
